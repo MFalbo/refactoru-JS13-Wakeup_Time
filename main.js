a9ba5531abc7099102b2dcbd42278e9c63c13419
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-15 21:34:19 -0600

    Elements added with jQuery.  Initial styling with CSS.

--- main.js
+++ main.js
@@ -6,4 +6,28 @@ $(document).ready(function(){
 	$('.container').append(outerShell);
 	outerShell.append(innerShell);
 
+	var pmLabel = $('<p class="pm-label">PM</p>');
+	var autoLabel = $('<p class="auto-label">AUTO</p>');
+	var clockScreen = $('<div class="clock-screen"></div>');
+
+	innerShell.append(pmLabel);
+	innerShell.append(autoLabel);
+	innerShell.append(clockScreen);
+
+	var indicator = $('<div class="indicator"></div>');
+	var clockText = $('<p class="clock-text">12:17</p>');
+
+	clockScreen.append(indicator);
+	clockScreen.append(clockText);
+
+	var amLabel = $('<p class="am-label">AM</p>');
+	var amFreq = $('<p class="am-freq">53 60 70 90 110 140 170    KHz</p>');
+	var fmLabel = $('<p class="fm-label">FM</p>');
+	var fmFreq = $('<p class="fm-freq">88 92 96 102 106 108     MHz</p>');
+
+	innerShell.append(amLabel);
+	innerShell.append(amFreq);
+	innerShell.append(fmLabel);
+	innerShell.append(fmFreq);
+
 });
